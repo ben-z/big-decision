6d23b97d265681ce4d7661cd902b4940b6716b74
Corbin Muraro <corbinmuraro@gmail.com>
2017-01-21 06:30:39 -0500

    Very basic version of tab to advance
    Still need to:
    make "create presentation" green
    fix weird desciption on first slide
    layouts
    fix incorrect ul shift <--
    make prev and next slides clickable

--- js/main.js
+++ js/main.js
@@ -1,10 +1,22 @@
+var newElement = "<li class='current'>" +
+					"<div class='input-fields'>" +
+						"<input type='text' class='title' placeholder='Title'><textarea rows='4' cols='50' class='description' placeholder='Description'></textarea>" +
+					"</div>" +
+				 "</li>";
+
 $(document).ready(function() {
+	$('body').click(function() {
+
+	});
+
+
 	$('.description').keydown(function(e) {
 	    var code = e.keyCode || e.which;
 
 	    if (code === 9) {  
 	        e.preventDefault();
-	        alert('it works!');
+	        		var newSlide = $('ul').append(newElement);
+		$('ul').animate({ "left": "-=80%" }, "slow" );
 	    }
 	});
 });
